04e2648d1266958dee04bed711912631b6c7dcd1
DakotaFrico33 <marco.cjpascolo@gmail.com>
2021-11-24 16:14:29 +0800

    HW#1: split and rearrange steps to make algorithm more readable

--- homework/01_hist_eq/src/main.py
+++ homework/01_hist_eq/src/main.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+from numpy.lib.index_tricks import s_
+
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -30,75 +32,77 @@ def plt_bar(x, height, title='title', xlabel='x', ylabel='y'):
 
 
 def main():
-    # Step 0.1
+    # Load image
     cwd = os.getcwd()
     assert os.path.exists(os.path.join(cwd,args.image))
     original = cv2.imread(args.image)
     gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 
-    # Step 0.2
     gray_flat = gray.flatten()
+    image_out_flat = np.zeros(gray_flat.shape, dtype=np.uint8)
+
     logger.debug(original.shape)
     logger.debug(gray.shape)
     logger.debug(gray_flat.shape)
 
     logger.debug(f'MIN: {min(gray_flat)}, MAX: {max(gray_flat)}, bits: {args.bits}, N: {2**args.bits}')
     if 2**args.bits < max(gray_flat):
-        logger.error('increase number of bits')
+        logger.error('incorrect number of bits')
         return
 
-    # Step 0.3
-    r_i = np.arange(0,2**args.bits)
+    # Step 1. Define range of intensities
+    r_i = np.arange(0,2**args.bits, dtype=np.uint8)
     r_i_count = np.zeros(r_i.shape, dtype=np.uint64)
-    r_sum = np.zeros(r_i_count.shape, dtype=np.uint64)
     s_i = np.zeros(r_i_count.shape, dtype=np.uint8)
     s_i_count = np.zeros(r_i.shape, dtype=np.uint64)
 
-    # Step 1. Count occurences of same pixel value
+    # Step 2. Count occurences of pixels with same intensity value
     for pixel in gray_flat:
         r_i_count[pixel] += 1
+
     try:
         logger.debug(f'MIN: {min(gray_flat)} --> {min(gray_flat)-1}|{r_i_count[min(gray_flat)-1]}, {min(gray_flat)}|{r_i_count[min(gray_flat)]}')
         logger.debug(f'MAX: {max(gray_flat)} --> {max(gray_flat)}|{r_i_count[max(gray_flat)]}, {max(gray_flat)+1}|{r_i_count[max(gray_flat)+1]}')
     except IndexError as e:
         logger.debug(e)
 
-    # Step 2. Sum (cumulative)
-    r_sum += np.arange(0,2**args.bits, dtype=np.uint64)*r_i_count
-    r_cumsum = r_sum.cumsum()
+    # Step 3. Calculate probability of occurrence for each intensity level
+    p_r = r_i_count / sum(r_i_count)
 
-    # Step 3. Create transformation function: map original pixel value to new pixel value
-    s_i = r_cumsum * (2**args.bits-1) // (sum(gray_flat))
+    # Step 4. Cumulative sum
+    r_cumsum = p_r.cumsum()
+
+    # Step 5. Mapping of pixel intensities between the original image and the processed one
+    s_i = r_cumsum * (2**args.bits-1)
+    np.rint(s_i)
+
     logger.debug(f'range gray_flat: {min(gray_flat)} - {max(gray_flat)}')
     logger.debug(f'range s_i: {min(s_i)} - {max(s_i)}')
     if args.debug and not args.no_plot:
         plt_plot(r_i,s_i,title='transformation function', xlabel='r_i', ylabel='s_i')
+        plt.show()
 
-    # Step 4. Apply transformation function
-    gray_flat_hist_eq = np.zeros(gray_flat.shape, dtype=np.uint8)
+    # Apply transformation function
     for ii,pixel in enumerate(gray_flat):
-        gray_flat_hist_eq[ii] = s_i[np.where(r_i == pixel)]
+        image_out_flat[ii] = s_i[np.where(r_i == pixel)]
 
-    # Step 5.
-    for pixel in gray_flat_hist_eq:
+    for pixel in image_out_flat:
         s_i_count[pixel] += 1
 
-
-    # Show images along with their respective histograms
+    # Save images and their respective histograms
     save_dir = args.image.rstrip('.tif')
     if args.local:
         save_dir += '/local'
 
-    cv2.imwrite(f"{save_dir}/0.png",gray)
-
-    image_out = gray_flat_hist_eq.reshape(gray.shape)
+    image_out = image_out_flat.reshape(gray.shape)
     logger.debug(image_out.shape)
     logger.debug(gray.shape)
+    cv2.imwrite(f"{save_dir}/0.png",gray)
     cv2.imwrite(f"{save_dir}/1.png",image_out)
 
-    fig = plt_bar(r_i,r_i_count, title='histogram BEFORE equalization', xlabel='intensity value', ylabel='number of pixels')
+    fig = plt_bar(r_i,r_i_count, title='Histogram of original image', xlabel='intensity value', ylabel='number of pixels')
     fig.savefig(f'{save_dir}/2.png')
-    fig = plt_bar(r_i,s_i_count, title='histogram AFTER equalization', xlabel='intensity value', ylabel='number of pixels')
+    fig = plt_bar(r_i,s_i_count, title='Histogram of processed image', xlabel='intensity value', ylabel='number of pixels')
     fig.savefig(f'{save_dir}/3.png')
 
     return
