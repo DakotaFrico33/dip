060c94a3b38b1e29121c360e67f4193d5f0e8be2
Marco Pascolo <m11007805@gapps.ntust.edu.tw>
2021-12-17 18:17:11 +0800

    Initialize VQ and Wavelet algorithm

---
+++ homework/02_compression/src/compression/vq.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Implement Linde-Buzo-Gray (LBG) algorithm to perform Vector Quantization
+
+def lbg():
+    pass
---
+++ homework/02_compression/src/compression/wavelet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Implementation of wavelet transform
+
+def wavelet():
+    pass
