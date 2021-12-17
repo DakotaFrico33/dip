fa2a356b3f982b0a3502619edee70c963548911e
Marco Pascolo <m11007805@gapps.ntust.edu.tw>
2021-12-17 17:26:59 +0800

    Array of DCT blocks defined as 1D array instead of 2D for simplicity

--- homework/02_compression/src/compression/dct.py
+++ homework/02_compression/src/compression/dct.py
@@ -9,20 +9,34 @@ from logger_setup import logger
 # + Fast DCT Algorithm
 
 def _split_into_blocks(img, block_size=2):
-    blocks=np.zeros((img.shape[0]//block_size,
-                    img.shape[1]//block_size,
+    blocks=np.ones((img.shape[0]*img.shape[1]//(block_size**2),
                     block_size,
-                    block_size),dtype=np.uint8)
+                    block_size),dtype=np.uint8)*(-1)
     logger.debug(blocks.shape)
+
+    if img.shape[0] % block_size != 0:
+        logger.error("Invalid block size: sizes of image and DCT block do not match")
+
+    if img.shape[1] % block_size != 0:
+        logger.error("Invalid block size: sizes of image and DCT block do not match")
 
     for j,row in enumerate(range(0,img.shape[0],block_size)):
         for k,col in enumerate(range(0,img.shape[1],block_size)):
             block = img[row:row+block_size, col:col+block_size]
-            blocks[j,k] = block
+            idx = j*(img.shape[1]//block_size)+k
+            blocks[idx] = block
     return blocks
 
 def dct_2d(img, args):
     blocks = _split_into_blocks(img, block_size=8)
+    for block in blocks:
+        for row in block:
+            for num in row:
+                pass
+            pass
+        pass
+    print(block)
+    print(block.shape)
     return img
 
 def idct_2d():
